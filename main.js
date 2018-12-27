addfe835f2da1be7de4ae15f0ee1336f7486079c
innova67 <37458981+innova67@users.noreply.github.com>
2018-12-27 00:31:33 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+function loadXMLDoc()
+{
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// para IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// para IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","info.txt",true);
+xmlhttp.setRequestHeader("Cache-control", "no-cache");
+xmlhttp.setRequestHeader("Cache-control", "no-store");
+
+xmlhttp.setRequestHeader("Cache-control", "max-age=100");
+xmlhttp.send();
+}
